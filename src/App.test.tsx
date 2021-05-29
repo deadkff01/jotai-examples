9533c31ab8fc5b71ead3e0e352ce9ff4886f0e03
deadkff01 <kafferdennis@gmail.com>
2021-05-29 20:17:51 -0300

    feat: initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("initial value", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const countValue = await screen.findByTestId("count-value");
+  expect(countValue.textContent).toBe("0");
+});
+
+test("increase", async () => {
+  render(<App />);
+  const btnIncrease = await screen.findByTestId("btn-increase");
+  userEvent.click(btnIncrease);
+  const countValue = await screen.findByTestId("count-value");
+  expect(countValue.textContent).toBe("1");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
 import React from "react";
-import { tw } from "twind";
-import logo from "static/logo.svg";
+// import { tw } from "twind";
+// import logo from "static/logo.svg";
 import GlobalStyles from "components/GlobalStyles";
 import "./App.css";
+import { atom, useAtom } from "jotai";
+import { countAtom } from "atoms/counter";
+
+const moreFiveAtom = atom((get) => get(countAtom) + 5);
 
 function App() {
+  const [count, setCount] = useAtom(countAtom);
+  const [moreFive] = useAtom(moreFiveAtom);
   return (
     <>
       <GlobalStyles />
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className={`${tw`font-bold`}`}>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 data-testid="count-value">{count}</h1>
+        <h1>{moreFive}</h1>
+        <button
+          data-testid="btn-increase"
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+          increase
+        </button>
+        <button onClick={() => setCount((prev) => prev - 1)}>decrease</button>
       </div>
     </>
   );
